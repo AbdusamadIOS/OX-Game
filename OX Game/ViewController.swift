42dbbdb39ae56d2880430ff2e2bb92818d03e57f
abdusamad <abdusamad2609@gmail.com>
2024-01-21 19:18:36 +0500

    Initial Commit

---
+++ OX Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OX Game
+//
+//  Created by Abdusamad Mamasoliyev on 21/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
